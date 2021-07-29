65bc57f80b89dc609aaaaa3d854c54fb731b3110
lwtz <w.h@outlook.com.br>
2021-07-29 15:54:03 -0300

    createElement html / aula 4

--- js/index.js
+++ js/index.js
@@ -26,3 +26,57 @@ for (; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2)
     }
 }
+
+
+// Events JS
+
+let title = document.querySelector('.titulo')
+title.textContent = 'William é o melhor'
+title.addEventListener('click', () => {
+    console.log('Anon function')
+})
+// title.addEventListener('click', function (){
+//     console.log('Isso é uma funcao anonima')
+// })
+
+// function msg() {
+//     console.log('Eae')
+// }
+
+let btnAdd = document.querySelector('#adicionar-paciente')
+btnAdd.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    let form = document.querySelector('#form-add')
+
+    // let nome = form.nome.value
+    // let peso = form.peso.value
+    // let gordura = form.gordura.value
+    // let altura = form.altura.value
+    // console.log([nome, peso, gordura, altura])
+    // console.log({nome, peso, gordura, altura})
+
+    let pacienteTR = document.createElement('tr')
+
+    let pesoTD = document.createElement('td')
+    let alturaTD = document.createElement('td')
+    let nomeTD = document.createElement('td')
+    let gorduraTD = document.createElement('td')
+    let imcTD = document.createElement('td')
+
+    nomeTD.textContent = form.nome.value
+    pesoTD.textContent = form.peso.value
+    alturaTD.textContent = form.altura.value
+    gorduraTD.textContent = form.gordura.value
+
+    pacienteTR.appendChild(nomeTD)
+    pacienteTR.appendChild(pesoTD)
+    pacienteTR.appendChild(alturaTD)
+    pacienteTR.appendChild(gorduraTD)
+
+    console.log(pacienteTR)
+    let tabela = document.querySelector('#tabela-pacientes')
+    tabela.appendChild(pacienteTR)
+
+
+})
