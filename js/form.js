013a5ca5bf0c40b870a0a6c94fd2cfeca39d0dc6
lwtz <w.h@outlook.com.br>
2021-07-30 09:36:48 -0300

    created object Patient

--- js/form.js
+++ js/form.js
@@ -3,6 +3,8 @@ btnAdd.addEventListener('click', (event) => {
     event.preventDefault()
 
     let form = document.querySelector('#form-add')
+    let patient = gePatientForm(form)
+    console.log(patient)
 
     // let nome = form.nome.value
     // let peso = form.peso.value
@@ -31,9 +33,22 @@ btnAdd.addEventListener('click', (event) => {
     pacienteTR.appendChild(gorduraTD)
     pacienteTR.appendChild(imcTD)
 
-    console.log(pacienteTR)
+    // console.log(pacienteTR)
     let tabela = document.querySelector('#tabela-pacientes')
     tabela.appendChild(pacienteTR)
 
 
 })
+
+function gePatientForm(form) {
+    let patient;
+    return patient = {
+        name: form.nome.value,
+        weight: form.peso.value,
+        height: form.altura.value,
+        fat: form.gordura.value,
+        imc: calcIMC(form.peso.value, form.altura.value)
+
+    }
+
+}
